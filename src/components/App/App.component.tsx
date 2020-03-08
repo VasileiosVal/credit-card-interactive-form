b99c078c6d66cc8feacaf7a05c395d0fb7bfdec3
Vallas Vasileios <Vasileios.VALLAS@intrasoft-intl.com>
2020-03-09 00:34:16 +0200

    applied minor styling changes on inputs

--- src/components/App/App.component.tsx
+++ src/components/App/App.component.tsx
@@ -8,31 +8,41 @@ export const App: React.FC = () => (
       <div className="card-form">
         <div className="form-group">
           <label htmlFor="Card Number">Card Number</label>
-          <input className="input-element" type="text" value="number" />
+          <input className="input-element hover-faint" type="text" value="" />
         </div>
         <div className="form-group">
           <label htmlFor="Card Holder">Card Holder</label>
-          <input className="input-element" type="text" value="holder" />
+          <input className="input-element hover-faint" type="text" value="" />
         </div>
         <div className="inline-form-group">
           <div className="outer-select-group">
-            <label htmlFor="Card Holder">Card Holder</label>
+            <label htmlFor="Expiration Date">Expiration Date</label>
             <div className="inner-select-group">
-              <select className="select-element" value="month" name="" id="">
-                <option value="">hello</option>
+              <select
+                className="select-element hover-faint"
+                value="month"
+                name=""
+                id=""
+              >
+                <option value="">Month</option>
               </select>
-              <select className="select-element" value="day" name="" id="">
-                <option value="">hello</option>
+              <select
+                className="select-element hover-faint"
+                value="day"
+                name=""
+                id=""
+              >
+                <option value="">Year</option>
               </select>
             </div>
           </div>
           <div className="form-group">
-            <label htmlFor="Card Number">Card Number</label>
-            <input className="input-element" type="text" value="one" />
+            <label htmlFor="CVV">CVV</label>
+            <input className="input-element hover-faint" type="text" value="" />
           </div>
         </div>
         <div className="submit-group">
-          <button className="submit-button">Submit</button>
+          <button className="submit-button hover-faint">Submit</button>
         </div>
       </div>
     </div>
