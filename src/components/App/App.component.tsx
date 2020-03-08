31cb014ad34fdd49763770152ce08d1e9f3a39e2
Vallas Vasileios <Vasileios.VALLAS@intrasoft-intl.com>
2020-03-08 23:01:03 +0200

    finished initial card and form layout

--- src/components/App/App.component.tsx
+++ src/components/App/App.component.tsx
@@ -6,9 +6,34 @@ export const App: React.FC = () => (
     <div className="card-container">
       <div className="card-placeholder img1"></div>
       <div className="card-form">
-        <input type="text" value="one" />
-        <input type="text" value="one" />
-        <input type="text" value="one" />
+        <div className="form-group">
+          <label htmlFor="Card Number">Card Number</label>
+          <input className="input-element" type="text" value="number" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="Card Holder">Card Holder</label>
+          <input className="input-element" type="text" value="holder" />
+        </div>
+        <div className="inline-form-group">
+          <div className="outer-select-group">
+            <label htmlFor="Card Holder">Card Holder</label>
+            <div className="inner-select-group">
+              <select className="select-element" value="month" name="" id="">
+                <option value="">hello</option>
+              </select>
+              <select className="select-element" value="day" name="" id="">
+                <option value="">hello</option>
+              </select>
+            </div>
+          </div>
+          <div className="form-group">
+            <label htmlFor="Card Number">Card Number</label>
+            <input className="input-element" type="text" value="one" />
+          </div>
+        </div>
+        <div className="submit-group">
+          <button className="submit-button">Submit</button>
+        </div>
       </div>
     </div>
   </div>
