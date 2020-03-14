586dcee91742228b955daaf798f41ef2f307c23e
Vallas Vasileios <Vasileios.VALLAS@intrasoft-intl.com>
2020-03-14 02:43:02 +0200

    applied flip functionality

--- src/components/Card/Card.component.tsx
+++ src/components/Card/Card.component.tsx
@@ -36,23 +36,39 @@ export const Card: React.FC = () => {
   };
 
   return (
-    <div className={`${styles.cardPlaceholder} ${styles.img1}`}>
-      <div className={styles.chip}></div>
-      <div className={styles.type}></div>
-      <div className={styles.number}>
-        <div className={styles.numberHolder}>{generateNumberHolder()}</div>
-      </div>
-      <div className={styles.nameContainer}>
-        <label className={styles.nameLabel} htmlFor="Card Holder">
-          Card Holder
-        </label>
-        <div className={styles.name}>Full Name</div>
-      </div>
-      <div className={styles.expireContainer}>
-        <label className={styles.nameLabel} htmlFor="Expires">
-          Expires
-        </label>
-        <div className={styles.expire}>MM/YY</div>
+    <div className={styles.cardOuterContainer}>
+      <div className={`${styles.cardInnerContainer}`}>
+        <div className={`${styles.frontCardPlaceholder} ${styles.img1}`}>
+          <div className={styles.chip}></div>
+          <div className={styles.type}></div>
+          <div className={styles.number}>
+            <div className={styles.numberHolder}>{generateNumberHolder()}</div>
+          </div>
+          <div className={styles.nameContainer}>
+            <label className={styles.nameLabel} htmlFor="Card Holder">
+              Card Holder
+            </label>
+            <div className={styles.name}>Full Name</div>
+          </div>
+          <div className={styles.expireContainer}>
+            <label className={styles.nameLabel} htmlFor="Expires">
+              Expires
+            </label>
+            <div className={styles.expire}>mm/yy</div>
+          </div>
+        </div>
+        <div
+          className={`${styles.backCardPlaceholder} ${styles.img1} ${styles.cardRotate}`}
+        >
+          <div className={styles.line}></div>
+          <div className={styles.cvvContainer}>
+            <label className={styles.cvvLabel} htmlFor="cvv">
+              cvv
+            </label>
+            <div className={styles.cvvOutput}>***</div>
+          </div>
+          <div className={styles.type}></div>
+        </div>
       </div>
     </div>
   );
