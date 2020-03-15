98001e08900c83b4f877d71405a3b562e9887ae8
Vallas Vasileios <Vasileios.VALLAS@intrasoft-intl.com>
2020-03-15 16:39:57 +0200

    changed ts config to allow iterator - applied linting - folder restructure

--- src/components/App/App.component.tsx
+++ src/components/App/App.component.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Card } from "../Card/Card.component";
 import { Form } from "../Form/Form.component";
-import styles from './App.module.scss'
+import styles from "./App.module.scss";
 
 export const App: React.FC = () => (
   <div className={styles.appContainer}>
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {App} from './components/App/App.component';
-import './styles/index.scss'
+import React from "react";
+import ReactDOM from "react-dom";
+import { App } from "./components/App/App.component";
+import "./styles/global/global.styles.scss";
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
