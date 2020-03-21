c5eb56d67808a72d952a3a1f495021d34bd5783c
Vallas Vasileios <Vasileios.VALLAS@intrasoft-intl.com>
2020-03-21 02:14:42 +0200

    feed components with mobx state

--- src/components/App/App.component.tsx
+++ src/components/App/App.component.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Card } from "../Card/Card.component";
-import { Form } from "../Form/Form.component";
+import Form from "../Form/Form.component";
 import styles from "./App.module.scss";
 
 export const App: React.FC = () => (
--- src/components/Form/Form.component.tsx
+++ src/components/Form/Form.component.tsx
@@ -1,73 +1,29 @@
-import React from "react";
+import React, { useContext } from "react";
+import { observer } from "mobx-react-lite";
 import { Input } from "../reusable/Input/Input.component";
 import { Select } from "../reusable/Select/Select.component";
 import { Label } from "../reusable/Label/Label.component";
+import RootContext, { RootStore } from "../../stores/rootStore";
 import styles from "./Form.module.scss";
 
-// *** think of new place
-const monthNumbers = [
-  "Month",
-  ...[...Array(12).keys()].map(val => `${val + 1}`)
-];
+const Form: React.FC = () => {
+  const { infoStore } = useContext<RootStore>(RootContext);
+  const {
+    months,
+    years,
+    formCvv,
+    formMonth,
+    formName,
+    formNumber,
+    formYear
+  } = infoStore;
 
-const yearNumbers = [
-  "Year",
-  ...[...Array(10).keys()].map(val => `${new Date().getFullYear() + val}`)
-];
-//***
-
-export const Form: React.FC = () => (
-  <div className={styles.cardForm}>
-    <Input
-      label="Card Number"
-      name="number"
-      value=""
-      onChange={() => {}}
-      onBlur={() => {}}
-      onFocus={() => {}}
-      addContainerStyles={["formGroup"]}
-      addLabelStyles={["label"]}
-      addInputStyles={["inputElement", "hoverFaint"]}
-    />
-    <Input
-      label="Card Holder"
-      name="name"
-      value=""
-      onChange={() => {}}
-      onBlur={() => {}}
-      onFocus={() => {}}
-      addContainerStyles={["formGroup"]}
-      addLabelStyles={["label"]}
-      addInputStyles={["inputElement", "hoverFaint"]}
-    />
-    <div className={styles.inlineFormGroup}>
-      <div className={styles.outerSelectGroup}>
-        <Label label="Expiration Date" addLabelStyles={["label"]} />
-        <div className={styles.innerSelectGroup}>
-          <Select
-            name="month"
-            value="Month"
-            onChange={() => {}}
-            onBlur={() => {}}
-            onFocus={() => {}}
-            data={monthNumbers}
-            addSelectStyles={["selectElement", "hoverFaint"]}
-          />
-          <Select
-            name="year"
-            value="Year"
-            onChange={() => {}}
-            onBlur={() => {}}
-            onFocus={() => {}}
-            data={yearNumbers}
-            addSelectStyles={["selectElement", "hoverFaint"]}
-          />
-        </div>
-      </div>
+  return (
+    <div className={styles.cardForm}>
       <Input
-        label="CVV"
-        name="cvv"
-        value=""
+        label="Card Number"
+        name="number"
+        value={formNumber}
         onChange={() => {}}
         onBlur={() => {}}
         onFocus={() => {}}
@@ -75,13 +31,64 @@ export const Form: React.FC = () => (
         addLabelStyles={["label"]}
         addInputStyles={["inputElement", "hoverFaint"]}
       />
+      <Input
+        label="Card Holder"
+        name="name"
+        value={formName}
+        onChange={() => {}}
+        onBlur={() => {}}
+        onFocus={() => {}}
+        addContainerStyles={["formGroup"]}
+        addLabelStyles={["label"]}
+        addInputStyles={["inputElement", "hoverFaint"]}
+      />
+      <div className={styles.inlineFormGroup}>
+        <div className={styles.outerSelectGroup}>
+          <Label label="Expiration Date" addLabelStyles={["label"]} />
+          <div className={styles.innerSelectGroup}>
+            <Select
+              name="month"
+              value={formMonth}
+              disabled={months[0]}
+              onChange={() => {}}
+              onBlur={() => {}}
+              onFocus={() => {}}
+              data={months}
+              addSelectStyles={["selectElement", "hoverFaint"]}
+            />
+            <Select
+              name="year"
+              value={formYear}
+              disabled={years[0]}
+              onChange={() => {}}
+              onBlur={() => {}}
+              onFocus={() => {}}
+              data={years}
+              addSelectStyles={["selectElement", "hoverFaint"]}
+            />
+          </div>
+        </div>
+        <Input
+          label="CVV"
+          name="cvv"
+          value={formCvv}
+          onChange={() => {}}
+          onBlur={() => {}}
+          onFocus={() => {}}
+          addContainerStyles={["formGroup"]}
+          addLabelStyles={["label"]}
+          addInputStyles={["inputElement", "hoverFaint"]}
+        />
+      </div>
+      <Input
+        type="button"
+        name="submit"
+        value="Submit"
+        addContainerStyles={["submitGroup"]}
+        addInputStyles={["submitButton", "hoverFaint"]}
+      />
     </div>
-    <Input
-      type="button"
-      name="submit"
-      value="Submit"
-      addContainerStyles={["submitGroup"]}
-      addInputStyles={["submitButton", "hoverFaint"]}
-    />
-  </div>
-);
+  );
+};
+
+export default observer(Form);
