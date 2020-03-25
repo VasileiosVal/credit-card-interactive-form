86fa7fb9c086e4113e482ca41af5fefb5c8dd3e8
Vallas Vasileios <Vasileios.VALLAS@intrasoft-intl.com>
2020-03-25 18:05:01 +0200

    used display computed properties to generate card front display data

--- src/components/Card/Card.component.tsx
+++ src/components/Card/Card.component.tsx
@@ -1,8 +1,9 @@
-import React, { useMemo, useContext } from "react";
+import React, { useContext } from "react";
 import { observer } from "mobx-react-lite";
 import RootContext, { RootStore } from "../../stores/rootStore";
 import { Label } from "../reusable/Label/Label.component";
 import { Letter } from "../reusable/Letter/Letter.component";
+import { initialName } from "../../stores/cardDisplay/displayStore";
 import styles from "./Card.module.scss";
 
 type DivElement = React.ReactElement<HTMLDivElement>;
@@ -10,16 +11,34 @@ type DivElementArray = DivElement[];
 
 const Card: React.FC = () => {
   const { displayStore } = useContext<RootStore>(RootContext);
-  const { transformCardNumber } = displayStore;
+  const {
+    generateCardNumber,
+    generateCardName,
+    generateCardMonth,
+    generateCardYear
+  } = displayStore;
 
-  const renderCardNumber = useMemo(() => {
+  const renderData = (param: string) => {
+    switch (param) {
+      case "number":
+        return transformNumber(generateCardNumber);
+      case "name":
+        return transformName(generateCardName);
+      case "month":
+        return transformDate(generateCardMonth);
+      case "year":
+        return transformDate(generateCardYear);
+    }
+  };
+
+  const transformNumber = (givenNumber: string) => {
     const elementArr: DivElementArray = [];
 
     for (let i = 0; i < 4; i++) {
-      const quarterNumHolder: DivElementArray = transformCardNumber
+      const quarterNumHolder: DivElementArray = givenNumber
         .slice(i * 4, i * 4 + 4)
         .split("")
-        .map((value, i) => <Letter key={i} value={value} />);
+        .map((letter, i) => <Letter key={i} animate="number" value={letter} />);
 
       const quarterElement: DivElement = (
         <div className={styles.quarterHolder} key={i}>
@@ -31,45 +50,65 @@ const Card: React.FC = () => {
     }
 
     return elementArr;
-  }, [transformCardNumber]);
+  };
 
-  const frontCard = useMemo(
-    () => (
-      <div
-        className={`${styles.cardPlaceholder} ${styles.frontCardGrid} ${styles.img1}`}
-      >
-        <div className={`${styles.chipPlaceHolder} ${styles.chip}`}></div>
-        <div className={`${styles.typeHolder} ${styles.mastercard}`}></div>
-        <div className={styles.number}>
-          <div className={styles.numberHolder}>{renderCardNumber}</div>
-        </div>
-        <div className={styles.nameContainer}>
-          <Label label="Card Holder" addLabelStyles={["nameLabel"]} />
-          <div className={styles.name}>Full Name</div>
-        </div>
-        <div className={styles.expireContainer}>
-          <Label label="Expires" addLabelStyles={["nameLabel"]} />
-          <div className={styles.expire}>mm/yy</div>
+  const transformName = (givenName: string) => {
+    const elementArr: DivElementArray = givenName
+      .split("")
+      .map((letter, i) => (
+        <Letter
+          key={i}
+          animate={givenName === initialName ? "initial-name" : "name"}
+          value={letter}
+        />
+      ));
+
+    return elementArr;
+  };
+
+  const transformDate = (givenDate: string) => {
+    const elementArr: DivElementArray = givenDate
+      .split("")
+      .map((value, i) => <Letter key={i} animate="date" value={value} />);
+
+    return elementArr;
+  };
+
+  const frontCard = (
+    <div
+      className={`${styles.cardPlaceholder} ${styles.frontCardGrid} ${styles.img1}`}
+    >
+      <div className={`${styles.chipPlaceHolder} ${styles.chip}`}></div>
+      <div className={`${styles.typeHolder} ${styles.mastercard}`}></div>
+      <div className={styles.number}>
+        <div className={styles.numberHolder}>{renderData("number")}</div>
+      </div>
+      <div className={styles.nameContainer}>
+        <Label label="Card Holder" addLabelStyles={["nameLabel"]} />
+        <div className={styles.name}>{renderData("name")}</div>
+      </div>
+      <div className={styles.expireContainer}>
+        <Label label="Expires" addLabelStyles={["nameLabel"]} />
+        <div className={styles.expire}>
+          {renderData("month")}
+          <span>/</span>
+          {renderData("year")}
         </div>
       </div>
-    ),
-    [renderCardNumber]
+    </div>
   );
 
-  const backCard = useMemo(
-    () => (
-      <div
-        className={`${styles.cardPlaceholder} ${styles.backCardGrid} ${styles.img1} ${styles.cardRotate}`}
-      >
-        <div className={styles.line}></div>
-        <div className={styles.cvvContainer}>
-          <Label label="CVV" addLabelStyles={["cvvLabel"]} />
-          <div className={styles.cvvOutput}>553</div>
-        </div>
-        <div className={`${styles.backTypeHolder} ${styles.mastercard}`}></div>
+  const backCard = (
+    <div
+      className={`${styles.cardPlaceholder} ${styles.backCardGrid} ${styles.img1} ${styles.cardRotate}`}
+    >
+      <div className={styles.line}></div>
+      <div className={styles.cvvContainer}>
+        <Label label="CVV" addLabelStyles={["cvvLabel"]} />
+        <div className={styles.cvvOutput}>553</div>
       </div>
-    ),
-    []
+      <div className={`${styles.backTypeHolder} ${styles.mastercard}`}></div>
+    </div>
   );
 
   return (
