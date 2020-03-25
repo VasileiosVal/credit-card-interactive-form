c381d56fb8e8582e7a4690373feb9a46368068b6
Vallas Vasileios <Vasileios.VALLAS@intrasoft-intl.com>
2020-03-25 18:16:58 +0200

    changed logic on letter component regarding animation per category

--- src/components/reusable/Letter/Letter.component.tsx
+++ src/components/reusable/Letter/Letter.component.tsx
@@ -1,11 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Letter.module.scss";
 
 interface Props {
   value: string;
+  animate: string;
 }
-export const Letter: React.FC<Props> = ({ value }) => (
-  <div className={value === "#" ? styles.numLeave : styles.numEnter}>
-    {value}
-  </div>
-);
+
+export const Letter: React.FC<Props> = ({ value, animate }) => {
+  const [animation, setAnimation] = useState("");
+
+  const animateClass = (value: string, animate: string) => {
+    switch (animate) {
+      case "number":
+        return value === "#" ? styles.valueLeave : styles.valueEnter;
+
+      case "name":
+        return styles.valueJoinRight;
+
+      case "initial-name":
+        return styles.valueEnter;
+
+      case "date":
+        return Number.isInteger(parseInt(value))
+          ? styles.valueEnter
+          : styles.valueLeave;
+
+      default:
+        return "";
+    }
+  };
+
+  useEffect(() => {
+    setAnimation(animateClass(value, animate));
+  }, [value, animate]);
+
+  useEffect(() => {
+    animation && setTimeout(() => setAnimation(""), 200);
+  }, [animation]);
+
+  return (
+    <div className={animation}>
+      {value === " " ? <span>&nbsp;</span> : value}
+    </div>
+  );
+};
